cd6b64a677c452c9254a2fc1c884503b5db33ccd
Sebastian Boeder <13516762+BoeseB@users.noreply.github.com>
2025-05-28 07:06:01 +0200

    Add application insights to blazor app

--- WeatherUI/Program.cs
+++ WeatherUI/Program.cs
@@ -1,3 +1,5 @@
+using BlazorApplicationInsights;
+using BlazorApplicationInsights.Models;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using WeatherUI;
@@ -6,6 +8,24 @@ using WeatherUI.Services;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddBlazorApplicationInsights(config =>
+{
+    config.ConnectionString = "{Insert Connection String}";
+},
+async applicationInsights =>
+{
+    var telemetryItem = new TelemetryItem()
+    {
+        Tags = new Dictionary<string, object?>()
+        {
+            { "ai.cloud.role", "SPA" },
+            { "ai.cloud.roleInstance", "Blazor Wasm" },
+        }
+    };
+
+    await applicationInsights.AddTelemetryInitializer(telemetryItem);
+});
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<WeatherService>();
